0cd2653af016ed28cfbc8129a6bf3cccc6f84041
muzudho <muzudho1@gmail.com>
2025-01-08 20:12:44 +0900

    ruler

--- examples/step1_render_empty.py
+++ examples/step1_render_empty.py
@@ -3,6 +3,7 @@
 """
 
 import openpyxl as xl
+from openpyxl.styles import PatternFill
 import json
 
 
@@ -40,6 +41,94 @@ for column_th in range(1, length_of_columns + 1):
 for row_th in range(1, length_of_rows + 1):
     ws.row_dimensions[row_th].height = 15    # 15 points = about 30 pixels
 
+# 上記の着色
+dark_gray = PatternFill(patternType='solid', fgColor="808080")
+light_gray = PatternFill(patternType='solid', fgColor="F2F2F2")
+# 定規の着色　＞　上辺
+row_th = 1
+for column_th in range(1, length_of_columns + 1):
+    column_letter = xl.utils.get_column_letter(column_th)
+    cell = ws[f'{column_letter}{row_th}']
+    
+    # -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    # --------- -------- -------- ---------
+    # dark      light    dark     light
+    #
+    # + 1 する
+    #
+    # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+    # -------- -------- -------- ----------
+    # dark     light    dark     light
+    #
+    # 3 で割って端数を切り捨て
+    #
+    # 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3,
+    # -------- -------- -------- --------
+    # dark     light    dark     light
+    #
+    # 2 で割った余り
+    #
+    # 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1,
+    # -------- -------- -------- --------
+    # dark     light    dark     light
+    #
+#     print(f"""\
+# column_th={column_th}
+# (column_th + 1)={(column_th + 1)}
+# (column_th + 1) // 3={(column_th + 1) // 3}
+# (column_th + 1) // 3 % 2={(column_th + 1) // 3 % 2}
+# """)
+    if (column_th + 1) // 3 % 2 == 0:
+        cell.fill = dark_gray
+    else:
+        cell.fill = light_gray
+
+# 定規の着色　＞　左辺
+column_th = 1
+for row_th in range(1, length_of_rows + 1):
+    column_letter = xl.utils.get_column_letter(column_th)
+    cell = ws[f'{column_letter}{row_th}']
+    
+    if (row_th + 1) // 3 % 2 == 0:
+        cell.fill = dark_gray
+    else:
+        cell.fill = light_gray
+
+# 定規の着色　＞　下辺
+row_th = length_of_rows
+bottom_is_dark_gray = (row_th + 1) // 3 % 2 == 0
+for column_th in range(1, length_of_columns + 1):
+    column_letter = xl.utils.get_column_letter(column_th)
+    cell = ws[f'{column_letter}{row_th}']
+    
+    if (column_th + 1) // 3 % 2 == 0:
+        if bottom_is_dark_gray:
+            cell.fill = dark_gray
+        else:
+            cell.fill = light_gray
+    else:
+        if bottom_is_dark_gray:
+            cell.fill = light_gray
+        else:
+            cell.fill = dark_gray
+
+# 定規の着色　＞　右辺
+column_th = length_of_rows
+rightest_is_dark_gray = (column_th + 1) // 3 % 2 == 0
+for row_th in range(1, length_of_rows + 1):
+    column_letter = xl.utils.get_column_letter(column_th)
+    cell = ws[f'{column_letter}{row_th}']
+    
+    if (row_th + 1) // 3 % 2 == 0:
+        if rightest_is_dark_gray:
+            cell.fill = dark_gray
+        else:
+            cell.fill = light_gray
+    else:
+        if rightest_is_dark_gray:
+            cell.fill = light_gray
+        else:
+            cell.fill = dark_gray
 
 # ワークブックの保存            
 wb.save('./temp/examples/step1_new_paper.xlsx')
